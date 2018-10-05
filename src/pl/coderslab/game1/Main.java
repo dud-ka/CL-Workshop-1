b80bd94d680582bef8877f0985f658a85b88fe85
Karolina Dudzik <dudzik.karolina@outlook.com>
2018-10-05 11:59:28 +0200

    random numbers

--- src/pl/coderslab/game1/Main.java
+++ src/pl/coderslab/game1/Main.java
@@ -1,4 +1,35 @@
 package pl.coderslab.game1;
 
+import java.util.Random;
+
 public class Main {
+
+	public static void main(String[] args) {
+
+		Random r = new Random();
+		int random = r.nextInt(100)+1;    //max = 100, min = 1
+
+		System.out.println(random);
+
+	}
+
+
+
+//	Napisz prostą grę w zgadywanie liczb. Komputer ma wylosować liczbę w zakresie od 1 do 100.
+//  Następnie:
+//      1. wypisać: "Zgadnij liczbę" i pobrać liczbę z klawiatury;
+//      2. sprawdzić, czy wprowadzony napis, to rzeczywiście liczba i w razie błędu wyświetlić komunikat: "To
+//          nie jest liczba"
+//          , po czym wrócić do pkt. 1;
+//      3. jeśli liczba podana przez użytkownika jest mniejsza niż wylosowana, wyświetlić komunikat: "Za
+//          mało!"
+//          , po czym wrócić do pkt. 1;
+//      4. jeśli liczba podana przez użytkownika jest większa niż wylosowana, wyświetlić komunikat: "Za
+//          dużo!"
+//          , po czym wrócić do pkt. 1;
+//      5. jeśli liczba podana przez użytkownika jest równa wylosowanej, wyświetlić komunikat: "Zgadłeś!"
+//          , po
+//          czym zakończyć działanie programu.
+
+
 }
