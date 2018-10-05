0dc00bf060ee54bfe167e72360d833f76a96770e
Karolina Dudzik <dudzik.karolina@outlook.com>
2018-10-05 15:49:58 +0200

    game2 - user choice

--- src/pl/coderslab/game2/Main.java
+++ src/pl/coderslab/game2/Main.java
@@ -5,12 +5,13 @@ import org.apache.commons.lang3.ArrayUtils;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args){
 		System.out.println(Arrays.toString(randomArr()));
-
+		System.out.println(Arrays.toString(userChoice()));
 	}
 
 	public static int[] randomArr() {
@@ -19,15 +20,44 @@ public class Main {
 
 		for(int i=0; i< array.length; i++) {
 			while(array[i]==0) {
-				int random = r.nextInt(49) + 1;
+				int random = r.nextInt(49)+1;
 				if (!ArrayUtils.contains(array, random)) {
 					array[i] = random;
 				}
 			}
-
 		}
 
 		return array;
 	}
+
+	public static int[] userChoice(){
+		Scanner scan = new Scanner(System.in);
+		int[] numbers = new int[6];
+		for (int i=0; i<6; i++) {
+			while (numbers[i] == 0) {
+				while (numbers[i] < 1 || numbers[i] > 49) {
+					System.out.print("Podaj liczbę nr " + (i + 1) + " z zakresu 1-49 ");
+					while (!scan.hasNextInt()) {
+						System.out.print("Podaj liczbę ");
+						scan.next();
+					}
+					int number = scan.nextInt();
+					if (!ArrayUtils.contains(numbers, number)) {
+						numbers[i] = number;
+					}
+				}
+			}
+		}
+		Arrays.sort(numbers);
+
+		return numbers;
+
+
+	}
+
+
+//	public static checking() {
+//
+//	}
 
 }
